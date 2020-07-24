51c7368e98830f343dda15a8c723166c41296dbc
anh tran <anh.tran@9thwonder.com>
2020-07-24 10:09:29 +0700

    #none: update custom event trigger

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -2,7 +2,7 @@
 module.exports.about = function(req, res) {
     res.render('generic-text', {
         title: 'Giới thiệu về GIST',
-        content: "Đồ án GIS ..............abc xyz........ Khoảng cách được tính bằng bán kính từ người dùng tới đại điẻm"
+        content: "Đồ án GIS ..............abc xyz........ Khoảng cách được tính bằng bán kính từ người dùng tới địa điểm"
     });
 };
 
